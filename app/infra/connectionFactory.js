2bb808f4c5f9bd5da3997f2e903e4d9aebc559e0
ttrmoreira <ttrmoreira@gmail.com>
2018-04-09 09:29:58 -0300

    Acerto de detalhes para rodar aplicação no Heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -20,6 +20,28 @@ function createDBConnection(){
             database: 'casadocodigo_nodejs_test'
         });
     }
+
+    // var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+    // var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+    // if (process.env.NODE_ENV == 'production') {
+    //     return mysql.createConnection({
+            
+    //         host: grupos[3],
+    //         user: grupos[1],
+    //         password: grupos[2],
+    //         database: grupos[4]
+    //     });
+    // }
+
+     if (process.env.NODE_ENV == 'production') {
+        return mysql.createConnection({
+            
+            host: 'us-cdbr-iron-east-05.cleardb.net',
+            user: 'b4e12adf3aec0d',
+            password: '9b5e8ddd',
+            database: 'heroku_3b4c7f3165dd067'
+        });
+    }
 }
 
 module.exports = function() {
