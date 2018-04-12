a68e8af8be89dce52f1bceaf3aa815b49bec8080
ttrmoreira <ttrmoreira@gmail.com>
2018-04-12 11:52:55 -0300

    Ajuste do arquivo home.js para identificar o problema de conexão que pode estar ocorrendo

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,12 +1,19 @@
 module.exports = function(app){
-	app.get("/",function(req,res){
+    app.get("/",function(req,res){
+
+
 		var connection = app.infra.connectionFactory();
 		var produtosDAO = new app.infra.ProdutosDAO(connection);
 		produtosDAO.lista(function(err, resultados){   
-			res.render('home/index',{livros:resultados});
+
+			if (err){
+				console.log('Erro!');
+				console.log(err);
+			} else {
+				res.render('home/index',{livros:resultados});
+				console.log('Abrindo a view...');
+			}
+
 		}); 
 		connection.end();
-	});
-
-
-}
+    });
